0bd4acf9722c50de5aca9ebf56905f3afd7d23ac
Yonatannnn <yonatanalebachew7@gmail.com>
2025-07-02 14:02:33 +0300

    debt usecase' implemented

--- usecase/debt_usecase.go
+++ usecase/debt_usecase.go
@@ -1,6 +1,10 @@
 package usecase
 
-import "brolend/domain"
+import (
+    "brolend/domain"
+    "errors"
+    "time"
+)
 
 type DebtUsecase struct {
     repo domain.DebtRepository
@@ -10,59 +14,99 @@ func NewDebtUsecase(repo domain.DebtRepository) *DebtUsecase {
     return &DebtUsecase{repo: repo}
 }
 
-// Implement domain.DebtUsecase interface
-
 func (u *DebtUsecase) CreateDebt(debt domain.Debt) (string, error) {
-    // TODO: business logic for creating a debt
-    return "", nil
+    // Set initial status and date
+    debt.Status = domain.RequestPending
+    debt.Date = time.Now().Unix()
+    return u.repo.Create(&debt)
 }
 
 func (u *DebtUsecase) AcceptDebt(debtID string, lenderID string) error {
-    // TODO: business logic for accepting a debt
-    return nil
+    debt, err := u.repo.FindByID(debtID)
+    if err != nil {
+        return err
+    }
+    if debt.LenderID != lenderID {
+        return errors.New("not authorized")
+    }
+    if debt.Status != domain.RequestPending {
+        return errors.New("debt is not pending")
+    }
+    return u.repo.UpdateStatus(debtID, domain.RequestAccepted)
 }
 
 func (u *DebtUsecase) RejectDebt(debtID string, lenderID string) error {
-    // TODO: business logic for rejecting a debt
-    return nil
+    debt, err := u.repo.FindByID(debtID)
+    if err != nil {
+        return err
+    }
+    if debt.LenderID != lenderID {
+        return errors.New("not authorized")
+    }
+    if debt.Status != domain.RequestPending {
+        return errors.New("debt is not pending")
+    }
+    return u.repo.UpdateStatus(debtID, domain.RequestRejected)
 }
 
 func (u *DebtUsecase) RequestPaidApproval(debtID string, borrowerID string) error {
-    // TODO: business logic for borrower requesting paid approval
-    return nil
+    debt, err := u.repo.FindByID(debtID)
+    if err != nil {
+        return err
+    }
+    if debt.BorrowerID != borrowerID {
+        return errors.New("not authorized")
+    }
+    if debt.Status != domain.RequestAccepted {
+        return errors.New("debt is not accepted")
+    }
+    return u.repo.UpdateStatus(debtID, domain.RequestPaid)
 }
 
 func (u *DebtUsecase) ApprovePayment(debtID string, lenderID string) error {
-    // TODO: business logic for lender approving payment
-    return nil
+    debt, err := u.repo.FindByID(debtID)
+    if err != nil {
+        return err
+    }
+    if debt.LenderID != lenderID {
+        return errors.New("not authorized")
+    }
+    if debt.Status != domain.RequestPaid {
+        return errors.New("payment not requested")
+    }
+    return u.repo.UpdateStatus(debtID, domain.Settled)
 }
 
 func (u *DebtUsecase) RejectPaymentRequest(debtID string, lenderID string) error {
-    // TODO: business logic for lender rejecting payment request
-    return nil
+    debt, err := u.repo.FindByID(debtID)
+    if err != nil {
+        return err
+    }
+    if debt.LenderID != lenderID {
+        return errors.New("not authorized")
+    }
+    if debt.Status != domain.RequestPaid {
+        return errors.New("payment not requested")
+    }
+    return u.repo.UpdateStatus(debtID, domain.RequestAccepted) // revert to accepted
 }
 
 func (u *DebtUsecase) GetNetAmounts(userID string) (map[domain.Currency]float64, error) {
-    // TODO: business logic for net amounts
-    return nil, nil
+    return u.repo.FindNetAmounts(userID)
 }
 
 func (u *DebtUsecase) GetHistory(userID string) ([]*domain.Debt, error) {
-    // TODO: business logic for user debt history
-    return nil, nil
+    return u.repo.FindHistory(userID)
 }
 
 func (u *DebtUsecase) GetActiveIncoming(lenderID string) ([]*domain.Debt, error) {
-    // TODO: business logic for active incoming debts
-    return nil, nil
+    return u.repo.FindActiveIncoming(lenderID)
 }
 
 func (u *DebtUsecase) GetActiveOutgoing(borrowerID string) ([]*domain.Debt, error) {
-    // TODO: business logic for active outgoing debts
-    return nil, nil
+    return u.repo.FindActiveOutgoing(borrowerID)
 }
 
 func (u *DebtUsecase) GetIncomingRequests(lenderID string) ([]*domain.Debt, error) {
-    // TODO: business logic for incoming requests
-    return nil, nil
+    return u.repo.FindIncomingRequests(lenderID)
 }
